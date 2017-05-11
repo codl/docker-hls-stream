ec5bdb6884baac5069e2b728a81e928aa0290701
codl <codl@codl.fr>
2017-05-11 18:41:21 +0200

    fix broken variable declaration

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-set PLAYLIST=$(youtube-dl -f 95 -g "$1")
+PLAYLIST=$(youtube-dl -f 95 -g "$1")
 
 ffmpeg -i $PLAYLIST -c:a copy -c:v copy -f hls -hls_flags delete_segments -hls_list_size 40 stream.m3u8
 

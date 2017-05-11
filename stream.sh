c32333df15845d0ad9f9ca5ae040e703d94ba13e
codl <codl@codl.fr>
2017-05-11 18:37:23 +0200

    stream.sh please dont delete yoruself

--- stream.sh
+++ stream.sh
@@ -6,4 +6,4 @@ set PLAYLIST=$(youtube-dl -f 95 -g "$1")
 
 ffmpeg -i $PLAYLIST -c:a copy -c:v copy -f hls -hls_flags delete_segments -hls_list_size 40 stream.m3u8
 
-rm stream*
+rm stream*.ts stream.m3u8

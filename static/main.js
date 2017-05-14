ab7625095e4a6f040db8022627be6626c2d0d3c3
codl <codl@codl.fr>
2017-05-14 20:08:19 +0200

    whoops update to the proper version of main.js

--- static/main.js
+++ static/main.js
@@ -1,10 +1,11 @@
 if(Hls.isSupported()) {
     let video = document.getElementById('video');
     let hls = new Hls();
-    let streamname = window.location.search.slice(1) || "stream"
-    hls.loadSource(`${streamname}.m3u8`);
+    let stream = window.location.search.slice(1) || "stream";
+    hls.loadSource(`${stream}.m3u8`);
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.addEventListener("click", () => video.play());
         video.play();
     });
 }
